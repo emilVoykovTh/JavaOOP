dc83bbfe6f6f3df4bbbf6a78d20148fc6184c197
Emil Voykov <75309274+emilVoykovTh@users.noreply.github.com>
2021-03-13 19:14:28 +0200

    Adding new Resolutions

---
+++ src/WorkingWithAbstractions/CardSuit/CardSuit.java
@@ -0,0 +1,19 @@
+package WorkingWithAbstractions.CardSuit;
+
+public enum CardSuit {
+
+    CLUBS(0),
+    DIAMONDS(1),
+    HEARTS(2),
+    SPADES(3);
+
+    private final int ordinalValue;
+
+    CardSuit(int ordinalValue) {
+        this.ordinalValue = ordinalValue;
+    }
+
+    public int getOrdinalValue() {
+        return ordinalValue;
+    }
+}
---
+++ src/WorkingWithAbstractions/HotelReservation/DiscountType.java
@@ -0,0 +1,18 @@
+package WorkingWithAbstractions.HotelReservation;
+
+public enum DiscountType {
+
+    VIP(20),
+    SECOND_VISIT(10),
+    NONE(0);
+
+    private final int percent;
+
+    DiscountType(int percent) {
+        this.percent = percent;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+}
